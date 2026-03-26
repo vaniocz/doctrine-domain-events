666546ea94758351a22d248bb867451bf17057c5
David Vondrák <vondrak@voatt.cz>
2026-03-26 06:42:59 +0100

    Fix deprecations

--- src/Bundle/DependencyInjection/VanioDoctrineDomainEventsExtension.php
+++ src/Bundle/DependencyInjection/VanioDoctrineDomainEventsExtension.php
@@ -3,8 +3,8 @@ namespace Vanio\DoctrineDomainEvents\Bundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class VanioDoctrineDomainEventsExtension extends Extension
 {
@@ -12,7 +12,7 @@ class VanioDoctrineDomainEventsExtension extends Extension
      * @param mixed[] $configs
      * @param ContainerBuilder $container
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources'));
         $loader->load('services.yaml');
